f98b03622f6deb1c6364d513a8bbc151389d1ad1
minhln1284 <luong.ngoc.minh@sun-asterisk.com>
2022-09-25 16:14:06 +0700

    Still working fine #1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,16 @@
 class StaticPagesController < ApplicationController
   def index
     @products = Product.all.limit(4)
+    if !current_user.nil?
+      if !current_user.recommend.empty?
+        arr = current_user.recommend.split("-")
+        @recommendations = []
+        arr.each do |id|
+          item = Product.find_by(id: id.to_i)
+          @recommendations << item
+        end
+      end
+    end
   end
 
   def mens
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_175938) do
+ActiveRecord::Schema.define(version: 2022_09_25_045104) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -130,6 +130,8 @@ ActiveRecord::Schema.define(version: 2022_09_24_175938) do
     t.datetime "reset_sent_at"
     t.string "encrypted_password", default: "", null: false
     t.string "recommend"
+    t.string "reset_password_sent_at"
+    t.datetime "reset_password_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
